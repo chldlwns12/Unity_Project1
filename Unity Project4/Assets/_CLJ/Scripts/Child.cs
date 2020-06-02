adf56ab3272f7f438623bcbe80d6ae1668fb2b9f
chldlwns12 <chldlwns12@gmail.com>
2020-06-02 13:54:32 +0900

    2020.06.02 -불렛 클래스 생성 -총알 발사 -서브발사체 생성 -서브총알 발사

---
+++ Unity Project4/Assets/_CLJ/Scripts/Child.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Child : MonoBehaviour
+{
+    public GameObject[] child;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            for (int i = 0; i < child.Length; i++)
+            {
+                child[i].SetActive(true);
+            }
+        }
+    }
+}
---
+++ Unity Project4/Assets/_CLJ/Scripts/ChildFire.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildFire : MonoBehaviour
+{
+    public GameObject bulletFatory;
+
+    public GameObject bulletPoint;
+
+    bool isStart = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isStart)
+        {
+            StartCoroutine("Fire");
+        }
+    }
+
+    IEnumerator Fire()
+    {
+        isStart = false;
+
+        GameObject bullet = Instantiate(bulletFatory);
+        bullet.transform.position = bulletPoint.transform.position;
+
+        yield return new WaitForSeconds(0.5f);
+
+        isStart = true;
+    }
+}
